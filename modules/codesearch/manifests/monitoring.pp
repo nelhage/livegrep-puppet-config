67939d08b2f1f484b219e7b7f958a43c1336e403
Nelson Elhage <nelhage@nelhage.com>
2012-03-03 13:02:15 -0500

    Configure munin-node.

--- modules/codesearch/manifests/monitoring.pp
+++ modules/codesearch/manifests/monitoring.pp
@@ -2,4 +2,17 @@ class codesearch::monitoring {
   package { 'munin-node':
     ensure => 'installed'
   }
+
+  file { '/etc/munin/munin-node.conf':
+    source => 'puppet:///modules/codesearch/munin-node.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    notify => Service['munin-node']
+  }
+
+  service { 'munin-node':
+    ensure  => 'running',
+    require => Package['munin-node']
+  }
 }
