a1b4354bda20ade57a03fe29dbf79cf390ba60f8
Nelson Elhage <nelhage@nelhage.com>
2012-03-17 11:06:42 -0400

    Handle the ubuntu/root shenanigans

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,12 @@ set -ex
 here=$(dirname "$0")
 host="$1"
 
+if ! ssh -lroot "$host" echo SUCCESS | grep -q SUCCESS; then
+    ssh -lubuntu "$host" -- sh -ex <<EOF
+sudo sed -i 's/^.*\(ssh-rsa\)/\1/' /root/.ssh/authorized_keys
+EOF
+fi
+
 ssh -lroot $host -- sh -ex <<EOF
 apt-get update
 apt-get -y install puppet rsync </dev/null
