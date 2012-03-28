6e3cafcc6b59243568c08ac669b2a0a88aed69e6
Nelson Elhage <nelhage@nelhage.com>
2012-03-27 21:18:04 -0400

    Configure email.

--- modules/codesearch/files/config.local.js
+++ modules/codesearch/files/config.local.js
@@ -8,3 +8,9 @@ module.exports.SEARCH_ARGS = ["--load_index", path.join(linux, "codesearch.idx")
 module.exports.SEARCH_REF  = "v3.0";
 module.exports.BACKEND_CONNECTIONS = 8;
 module.exports.LOG4JS_CONFIG = path.join(__dirname, "log4js.codesearch.json");
+module.exports.SMTP_CONFIG = {
+  host: 'smtp.gmail.com',
+  user: 'mailer@livegrep.com',
+  ssl:   true,
+  password: '1fMq4aheht6U'
+};
