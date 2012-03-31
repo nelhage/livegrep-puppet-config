99c638dd4b3de1088101097e27cf0ad9744b5ea7
Nelson Elhage <nelhage@nelhage.com>
2012-03-31 08:06:58 -0400

    Download a different sw tarball based on ${lsbdistcodename}.

--- modules/codesearch/manifests/thirdparty_dl.pp
+++ modules/codesearch/manifests/thirdparty_dl.pp
@@ -1,15 +1,16 @@
 class codesearch::thirdparty_dl {
-  $sw_url = "https://livegrep.s3.amazonaws.com/3rdparty-sw.tar.gz"
+  $tgz    = "3rdparty-sw-${lsbdistcodename}.tar.gz"
+  $sw_url = "https://livegrep.s3.amazonaws.com/${tgz}"
   exec { "$sw_url":
     command => "wget -N ${sw_url}",
     cwd     => "/home/nelhage/",
-    creates => "/home/nelhage/3rdparty-sw.tar.gz",
+    creates => "/home/nelhage/${tgz}",
     user    => 'nelhage',
     path    => '/usr/bin:/bin'
   }
 
   exec { "unpack sw":
-    command => "tar xzf 3rdparty-sw.tar.gz",
+    command => "tar xzf ${tgz}",
     cwd     => "/home/nelhage/",
     creates => "/home/nelhage/sw/.installed",
     user    => 'nelhage',
