eed5d010256cdb62d9446c2fe3dbb7c3d09d29db
Nelson Elhage <nelhage@nelhage.com>
2012-11-29 16:09:08 -0800

    Bump version of node

--- modules/codesearch/manifests/thirdparty_apt.pp
+++ modules/codesearch/manifests/thirdparty_apt.pp
@@ -1,9 +1,10 @@
 class codesearch::thirdparty_apt {
   include apt::params
   apt::ppa { 'ppa:nelhage/livegrep': }
+  apt::ppa { 'ppa:chris-lea/node.js': }
   package { ['nodejs', 'npm', 're2-dev', 'libgit2-dev', 'gflags-dev']:
     ensure => latest,
-    require => Apt::Ppa['ppa:nelhage/livegrep'],
+    require => [Apt::Ppa['ppa:nelhage/livegrep'], Apt::Ppa['ppa:chris-lea/node.js']],
     before  => File['/home/nelhage/sw/.installed']
   }
 
