c6fdae9b53577b6bec82abd600011b05f239b620
Nelson Elhage <nelhage@nelhage.com>
2012-11-29 16:18:38 -0800

    No longer need to specify the index file in args.

--- modules/codesearch/files/config.local.js
+++ modules/codesearch/files/config.local.js
@@ -3,8 +3,7 @@ var path = require('path');
 var linux = '/home/nelhage/linux/';
 
 module.exports.SEARCH_REPO = linux;
-module.exports.SEARCH_ARGS = ["--load_index", path.join(linux, "codesearch.idx"),
-                              '--threads=4'];
+module.exports.SEARCH_ARGS = ['--threads=4'];
 module.exports.SEARCH_REF  = "refs/tags/v3.6";
 module.exports.BACKEND_CONNECTIONS = 8;
 module.exports.LOG4JS_CONFIG = path.join(__dirname, "log4js.codesearch.json");
